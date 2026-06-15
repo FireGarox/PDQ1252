156fe3cf11bdc26051d1fbb45b50dafe8666f769
FireGarox <105446564+FireGarox@users.noreply.github.com>
2026-06-15 13:43:31 +0700

    Add files via upload

---
+++ buffer.java
@@ -0,0 +1,51 @@
+public class buffer {
+    public static void main(String[] args) {
+        //No 2
+        System.out.println("soal no 2");
+        StringBuffer sb1 = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer(100);
+        StringBuffer sb3 = new StringBuffer("halo");
+        System.out.println(sb1 + " Panjang : " + sb1.length() + " Kapasitas : " + sb1.capacity());
+        System.out.println(sb2 + " Panjang : " + sb2.length() + " Kapasitas : " + sb2.capacity());
+        System.out.println(sb3 + " Panjang : " + sb3.length() + " Kapasitas : " + sb3.capacity());
+        
+        for (int i = 1; i <= 2 ; i++){
+            System.out.println();
+        }
+
+        //No 3
+        System.out.println("soal no 3");
+        StringBuffer ABC = new StringBuffer();
+        for (int i = 1; i <= 5 ; i++){
+            ABC.append("abc ");
+        }
+        System.out.println(ABC);
+
+        for (int i = 1; i <= 2 ; i++){
+            System.out.println();
+        }
+
+        //Contoh no 4
+        System.out.println("Contoh kondisi");
+        System.out.println("Kapasitas: " + ABC.capacity());
+        System.out.println("Panjang: " + ABC.length());
+
+        for (int i = 1; i <= 2 ; i++){
+            System.out.println();
+        }
+
+        //No 5
+        System.out.println("soal no 5");
+        String swapped = ("Swap");
+        System.out.println(swapped);
+        StringBuffer swap =  new StringBuffer();
+        swap.append(swapped);
+        System.out.println(swap);
+        swapped = swap.toString();
+        System.out.println(swapped);
+
+        
+
+    }
+    
+}
